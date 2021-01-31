f44f233dff334a1b3b1329d2de94afa3e659ac95
aditanov <aditanovlagusto@gmail.com>
2021-01-31 05:42:16 +0000

    push pertama

---
+++ moodledata/localcache/mustache/1611841206/moove/__Mustache_841bb773d133dce3f6e0b29e861285c5.php
@@ -0,0 +1,138 @@
+<?php
+
+class __Mustache_841bb773d133dce3f6e0b29e861285c5 extends Mustache_Template
+{
+    private $lambdaHelper;
+
+    public function renderInternal(Mustache_Context $context, $indent = '')
+    {
+        $this->lambdaHelper = new Mustache_LambdaHelper($this->mustache, $context);
+        $buffer = '';
+
+        $buffer .= $indent . '<div class="container-fluid">
+';
+        $buffer .= $indent . '    <div class="row row justify-content-center">
+';
+        $buffer .= $indent . '        <div class="col-8">
+';
+        $buffer .= $indent . '
+';
+        $buffer .= $indent . '            <div class="text-center">
+';
+        // 'logourl' section
+        $value = $context->find('logourl');
+        $buffer .= $this->sectionC38e675be42346e00e5b75d099c89a76($context, $indent, $value);
+        // 'logourl' inverted section
+        $value = $context->find('logourl');
+        if (empty($value)) {
+            
+            $buffer .= $indent . '                    <h2>';
+            $value = $this->resolveValue($context->find('sitename'), $context);
+            $buffer .= call_user_func($this->mustache->getEscape(), $value);
+            $buffer .= '</h2>
+';
+        }
+        $buffer .= $indent . '            </div>
+';
+        $buffer .= $indent . '
+';
+        $buffer .= $indent . '            <div class="card my-4">
+';
+        $buffer .= $indent . '                <div class="card-block">
+';
+        $buffer .= $indent . '                    <div class="card-title">
+';
+        $buffer .= $indent . '                        <h3>';
+        // 'str' section
+        $value = $context->find('str');
+        $buffer .= $this->section7011f9eedafcdb8d9dc662b124b2ed1e($context, $indent, $value);
+        $buffer .= '</h3>
+';
+        $buffer .= $indent . '                    </div>
+';
+        $buffer .= $indent . '                    ';
+        $value = $this->resolveValue($context->find('formhtml'), $context);
+        $buffer .= $value;
+        $buffer .= '
+';
+        $buffer .= $indent . '                </div>
+';
+        $buffer .= $indent . '            </div>
+';
+        $buffer .= $indent . '        </div>
+';
+        $buffer .= $indent . '    </div>
+';
+        $buffer .= $indent . '</div>
+';
+
+        return $buffer;
+    }
+
+    private function sectionC38e675be42346e00e5b75d099c89a76(Mustache_Context $context, $indent, $value)
+    {
+        $buffer = '';
+    
+        if (!is_string($value) && is_callable($value)) {
+            $source = '
+                    <h2><img src="{{logourl}}" title="{{sitename}}" alt="{{sitename}}"/></h2>
+                ';
+            $result = call_user_func($value, $source, $this->lambdaHelper);
+            if (strpos($result, '{{') === false) {
+                $buffer .= $result;
+            } else {
+                $buffer .= $this->mustache
+                    ->loadLambda((string) $result)
+                    ->renderInternal($context);
+            }
+        } elseif (!empty($value)) {
+            $values = $this->isIterable($value) ? $value : array($value);
+            foreach ($values as $value) {
+                $context->push($value);
+                
+                $buffer .= $indent . '                    <h2><img src="';
+                $value = $this->resolveValue($context->find('logourl'), $context);
+                $buffer .= call_user_func($this->mustache->getEscape(), $value);
+                $buffer .= '" title="';
+                $value = $this->resolveValue($context->find('sitename'), $context);
+                $buffer .= call_user_func($this->mustache->getEscape(), $value);
+                $buffer .= '" alt="';
+                $value = $this->resolveValue($context->find('sitename'), $context);
+                $buffer .= call_user_func($this->mustache->getEscape(), $value);
+                $buffer .= '"/></h2>
+';
+                $context->pop();
+            }
+        }
+    
+        return $buffer;
+    }
+
+    private function section7011f9eedafcdb8d9dc662b124b2ed1e(Mustache_Context $context, $indent, $value)
+    {
+        $buffer = '';
+    
+        if (!is_string($value) && is_callable($value)) {
+            $source = 'newaccount';
+            $result = call_user_func($value, $source, $this->lambdaHelper);
+            if (strpos($result, '{{') === false) {
+                $buffer .= $result;
+            } else {
+                $buffer .= $this->mustache
+                    ->loadLambda((string) $result)
+                    ->renderInternal($context);
+            }
+        } elseif (!empty($value)) {
+            $values = $this->isIterable($value) ? $value : array($value);
+            foreach ($values as $value) {
+                $context->push($value);
+                
+                $buffer .= 'newaccount';
+                $context->pop();
+            }
+        }
+    
+        return $buffer;
+    }
+
+}
